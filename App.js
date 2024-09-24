8484988ca691861d536863a61b2ab6689426c968
Kunja Salamanca <kunjabihari20@Gmail.com>
2024-09-25 01:17:35 +0800

    mob

--- App.js
+++ App.js
@@ -1,11 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Keyboard, KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task])
+    setTask(null);
+  }
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <View style={styles.tasksWrapper}>
+
+        <Text style={styles.sectionTitle}>Our to do list:</Text>
+
+        <View style={styles.items}>
+          {
+            taskItems.map((item, index) => {
+              return (
+                <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                  <Task text={item} />
+                </TouchableOpacity>
+              )
+            })
+          }
+
+      </View>
+      </View>
+
+      <KeyboardAvoidingView 
+        behavior={Platform.OS === "android" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}>
+          <TextInput style={styles.input} placeholder={'New Task'} value={task} onChangeText={text => setTask(text)} />
+
+          <TouchableOpacity onPress={() => handleAddTask()}>
+            <View style={styles.addWrapper}>
+              <Text style={styles.addtext}>+</Text>
+            </View>
+          </TouchableOpacity>
+
+        </KeyboardAvoidingView>
     </View>
   );
 }
@@ -13,8 +58,45 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#FFF8E8',
   },
+  tasksWrapper: {
+    paddingTop: 70,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  items: {
+    marginTop: 30,
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  input: {
+    padding: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#674636',
+    borderRadius: 40,
+    borderColor: '#FFF8E8',
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#674636',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems:'center',
+    borderColor: '#FFF8E8',
+    borderWidth: 1,
+  },
+  addtext: {},
 });
